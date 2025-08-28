8afdd265df978f7b6ab02ae3f215086edcae4ff5
0euun <jeyejong70@naver.com>
2025-08-28 21:50:33 +0900

    :recycle: BaseEntity 수정

--- src/main/java/com/example/reminder/ReminderApplication.java
+++ src/main/java/com/example/reminder/ReminderApplication.java
@@ -2,8 +2,10 @@ package com.example.reminder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ReminderApplication {
 
 	public static void main(String[] args) {
