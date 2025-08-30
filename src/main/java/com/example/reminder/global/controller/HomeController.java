8a6fb47369049f46f0682f3fb02b5cb2be909829
0euun <jeyejong70@naver.com>
2025-08-30 23:20:28 +0900

    :truck: 구조 수정

--- src/main/java/com/example/reminder/global/controller/HomeController.java
+++ src/main/java/com/example/reminder/global/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.reminder.global.entity.controller;
+package com.example.reminder.global.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
